ed1ea9437c1686265858c1b41a00fb5ab9ad4398
Tanmay Dubey <tanmaydubey1947@gmail.com>
2026-02-08 00:56:10 +0530

    Initial Commit

---
+++ src/main/java/com/fraud_analyzer/FraudPaymentAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.fraud_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudPaymentAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudPaymentAnalyzerApplication.class, args);
+	}
+
+}
