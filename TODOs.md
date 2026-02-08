a3be2cd2bb62a2ad411992b7d4581a3b166c58ef
Tanmay Dubey <tanmaydubey1947@gmail.com>
2026-02-08 20:30:40 +0530

    Ingestion service place-holder added

---
+++ TODOs.md
@@ -0,0 +1,8 @@
+Add below services
+
+Gateway: done
+Transaction Ingestion
+Fraud Detection - Core Engine
+Rule Management
+Decision Engine
+Audit & Reporting - future scope
---
+++ transaction-ingestion/src/main/java/com/fraud_analyzer/TransactionIngestionApplication.java
@@ -0,0 +1,13 @@
+package com.fraud_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionIngestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionIngestionApplication.class, args);
+	}
+
+}
